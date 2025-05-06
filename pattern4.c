bff87774101ff45402fefb2671dd15b9c564b492
Masrafi404 <mdmasrafi902@gmail.com>
2025-05-06 09:37:58 +0600

    pattern four

--- pattern4.c
+++ pattern4.c
@@ -3,6 +3,7 @@ int main()
 {
     int x;
     scanf("%d", &x);
+    int star = x * 2 - 1;
     int tmp = 0;
 
     for (int i = x; i >= 1; i--)
@@ -12,10 +13,11 @@ int main()
             printf(" ");
         }
         tmp++;
-        for (int j = 1; j <= i; j++)
+        for (int j = star; j >= 1; j--)
         {
             printf("*");
         }
+        star = star - 2;
         printf("\n");
     }
 
